39df2f4ae6c43953495f2e5116d2eb05a8f245b1
Twesh Lyallpuri <tweshlyallpuri@gmail.com>
2020-07-21 11:12:17 +0530

    Adding Demo files

---
+++ T4EmailTemplateDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T4EmailTemplateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailTemplate e = new EmailTemplate() { ListOfComponents = new List<EmailComponentTemplate>() { 
+                new EmailComponentTemplate() { Name = "Header_Twesh" }, 
+                new BodyTemplate() { FirstName = "Twesh", LastName = "Lyallpuri" }, 
+                new EmailComponentTemplate() { Name = "Footer_Twesh" } 
+            } };
+            var x = e.TransformText();
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
